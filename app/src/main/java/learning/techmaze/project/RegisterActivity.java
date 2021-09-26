38c1d80905cde19ca6f079ddd4c58a803ce1e1c4
dev-ojha <anubhavojha9@gmail.com>
2021-09-26 23:53:17 +0530

    mAuth method is set "create User with Email password"

--- app/src/main/java/learning/techmaze/project/RegisterActivity.java
+++ app/src/main/java/learning/techmaze/project/RegisterActivity.java
@@ -7,9 +7,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 
@@ -90,6 +95,23 @@ public class RegisterActivity extends AppCompatActivity {
            mLoadingBar.setMessage("Please wait");
            mLoadingBar.setCanceledOnTouchOutside(false);
            mLoadingBar.show();
+
+           mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+               @Override
+               public void onComplete(@NonNull Task<AuthResult> task) {
+                   if(task.isSuccessful()){
+                       Toast.makeText(RegisterActivity.this, "Succefully Registered", Toast.LENGTH_SHORT).show();
+
+                       Intent intent = new Intent(RegisterActivity.this,MainActivity.class);
+                       intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                       startActivity(intent);
+                   }
+                   else
+                   {
+                       Toast.makeText(RegisterActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                   }
+               }
+           });
         }
 
     }
