5ee307be87af9d4f032046052019df137095acf5
dev-ojha <anubhavojha9@gmail.com>
2021-09-26 20:32:11 +0530

    succesfully set click on listener on forgot password

--- app/src/main/java/learning/techmaze/project/LoginActivity.java
+++ app/src/main/java/learning/techmaze/project/LoginActivity.java
@@ -19,6 +19,7 @@ public class LoginActivity extends AppCompatActivity {
     EditText login_password ;
     Button btn_signIN ;
     //FirebaseAuth firebaseAuth;
+    TextView tv_forget_password ;
 
 
     @Override
@@ -26,14 +27,22 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         register = findViewById(R.id.tv_register_user);
-        login_email = findViewById(R.id.login_email);
+        login_email = findViewById(R.id.enter_emailToResetPassword);
         login_password = findViewById(R.id.login_password);
-        btn_signIN = findViewById(R.id.btn_signIN);
+        btn_signIN = findViewById(R.id.btn_ResetPassword);
+        tv_forget_password = findViewById(R.id.tv_forget_password);
 
         btn_signIN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 checkCredentials();
+            }
+        });
+
+        tv_forget_password.setOnClickListener(new View.OnClickListener() {   //switch to reset password activity
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,Forgot_Password.class));
             }
         });
 
