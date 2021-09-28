5124eea4af355dfb93787dda7b2c65f9e23f6f2e
ojha <anubhavojha9@gmail.com>
2021-09-28 19:41:46 +0530

    application is crashing on opening

--- app/src/main/java/Adapters/Excercise_Adapter.java
+++ app/src/main/java/Adapters/Excercise_Adapter.java
@@ -1,4 +1,59 @@
 package Adapters;
 
-public class Excercise_Adapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import Models.Exercise_Model;
+import learning.techmaze.project.R;
+
+public class Excercise_Adapter extends RecyclerView.Adapter<Excercise_Adapter.viewHolder>{
+
+    ArrayList<Exercise_Model> list ;
+    Context context ;
+
+    public Excercise_Adapter(ArrayList<Exercise_Model> lists, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.rv_item_view, parent,false);
+
+        return new viewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewHolder holder, int position) {
+
+       Exercise_Model model = list.get(position) ;
+       holder.rv_imageView.setImageResource(model.getPic());
+       holder.rv_textView.setText(model.getText());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class viewHolder extends RecyclerView.ViewHolder {
+
+      ImageView rv_imageView ;
+      TextView rv_textView ;
+      public viewHolder(@NonNull View itemView) {
+          super(itemView);
+          rv_imageView =  itemView.findViewById(R.id.rv_imageView);
+          rv_textView = itemView.findViewById(R.id.rv_textView);
+      }
+  }
 }
--- app/src/main/java/learning/techmaze/project/MainActivity.java
+++ app/src/main/java/learning/techmaze/project/MainActivity.java
@@ -1,17 +1,19 @@
 package learning.techmaze.project;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.WindowManager;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import Adapters.Excercise_Adapter;
+import Models.Exercise_Model;
 
 public class MainActivity extends AppCompatActivity {
 
-  //  private RecyclerView recyclerView ;
+      RecyclerView recyclerView ;
   //  RecyclerView.LayoutManager layoutManager ;
   //  RecyclerViewAdapter recyclerViewAdapter ;
 
@@ -22,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        recyclerView = findViewById(R.id.rv_navigation);
+
        // register = findViewById(R.id.tv_register_user) ;
       //  getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON);
 
@@ -30,12 +34,29 @@ public class MainActivity extends AppCompatActivity {
             //public void onClick(View v) {
               //  startActivity(new Intent(MainActivity.this,RegisterActivity.class));
 
-           // }
+           // }//layoutManager = new GridLayoutManager( this,2) ;
+        //        //recyclerView.setLayoutManager(layoutManager);
         //});
 
 
-       // recyclerView = findViewById(R.id.rv_navigation);
-        //layoutManager = new GridLayoutManager( this,2) ;
-        //recyclerView.setLayoutManager(layoutManager);
+
+        ArrayList<Exercise_Model> list = new ArrayList<>() ;
+
+        list.add(new Exercise_Model(R.drawable.chest1,"chest"));
+        list.add(new Exercise_Model(R.drawable.gym1,"workout"));
+        list.add(new Exercise_Model(R.drawable.gym2,"triceps"));
+        list.add(new Exercise_Model(R.drawable.gym3,"chest"));
+        list.add(new Exercise_Model(R.drawable.gym4,"shoulder"));
+        list.add(new Exercise_Model(R.drawable.gym5,"chest"));
+        list.add(new Exercise_Model(R.drawable.gym6,"legs"));
+        list.add(new Exercise_Model(R.drawable.gym5,"chest"));
+        list.add(new Exercise_Model(R.drawable.gym2,"body"));
+
+      //  Excercise_Adapter excercise_adapter = new Excercise_Adapter(list,this);
+   //     recyclerView.setAdapter(excercise_adapter);
+
+    //    LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+     //   recyclerView.setLayoutManager(layoutManager);
+
     }
 }
